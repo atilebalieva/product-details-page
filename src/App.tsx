05f9ae7e2f286fcc83474cad1932fc119a207d44
atilebalieva <aitilebalieva@gmail.com>
2024-12-03 22:19:05 -0600

    moved data folder to public.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Routes } from "react-router-dom";
 import Navigation from "./components/nav/navigation";
+import ProductPage from "./components/product/product-page";
 
 function App() {
   return (
     <div className="container mx-auto px-4">
       <div>
         <Navigation />
+        <ProductPage />
       </div>
     </div>
   );
